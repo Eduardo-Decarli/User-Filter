c24c0e30d931857447011695f5eb27a1ba04c6f6
Eduardo-Decarli <decarliedu@gmail.com>
2025-10-15 21:45:03 -0300

    feat: create structure and styles of our user detail list

--- src/app/components/components-module.ts
+++ src/app/components/components-module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UserDetails } from './user-details/user-details';
+import { AngularMaterialModule } from '../angular-material/angular-material-module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    UserDetails
+  ],
   imports: [
-    CommonModule
-  ], exports: []
+    CommonModule,
+    AngularMaterialModule
+  ], exports: [
+    UserDetails
+  ]
 })
 export class ComponentsModule { }
